4867b20e69c883ac374c4213b19ebf6959e720b8
ras0q <ras0q@users.noreply.github.com>
2023-10-25 22:50:08 +0900

    :sparkles: /register

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/traPtitech/go-traq"
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
@@ -26,19 +28,37 @@ func main() {
 	})
 
 	bot.OnMessageCreated(func(p *payload.MessageCreated) {
-		embed := true
-		_, _, err := bot.API().
-			MessageApi.
-			PostMessage(context.Background(), channelID).
-			PostMessageRequest(traq.PostMessageRequest{
-				Content: "@Ras",
-				Embed:   &embed,
-			}).
-			Execute()
-		if err != nil {
-			log.Println("Failed to post message:", err)
+		// ex: /register <traQ ID> <GitHub ID>
+		if strings.HasPrefix(p.Message.PlainText, "/register") {
+			args := strings.Split(p.Message.PlainText, " ")
+			if len(args) != 3 {
+				mustPostMessage(bot, "Usage: `/register <traQ ID> <GitHub ID>`")
+			}
+
+			traqID := args[1]
+			githubID := args[2]
+
+			fmt.Println(traqID, githubID) // TODO: register to DB
+
+			mustPostMessage(bot, "Registered!")
 		}
+
+		mustPostMessage(bot, "@Ras")
 	})
+}
+
+func mustPostMessage(bot *traqwsbot.Bot, content string) {
+	embed := true
+
+	_, _, err := bot.API().
+		MessageApi.
+		PostMessage(context.Background(), channelID).
+		PostMessageRequest(traq.PostMessageRequest{
+			Content: content,
+			Embed:   &embed,
+		}).
+		Execute()
+	panicOnError(err)
 }
 
 func panicOnError(err error) {
